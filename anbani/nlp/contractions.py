9fda0b47a1231412e85fa29d7c58a3f2b8ba36ae
alex <turdzeladze@gmail.com>
2025-01-16 14:05:10 +0400

    simple bug fix and text contractor

--- anbani/nlp/contractions.py
+++ anbani/nlp/contractions.py
@@ -7,6 +7,7 @@ module_path = Path(__file__).parent.parent.absolute()
 
 cdf = pd.read_csv(os.path.join(module_path, "data/georgian_contractions.csv"))
 cmap = cdf.set_index('CONTRACTION')[['EXPANSION']].to_dict('dict')['EXPANSION']
+emap = cdf.set_index('EXPANSION')[['CONTRACTION']].to_dict('dict')['CONTRACTION']
 
 def expand(word):
     # Just a wrapper around contractions map
@@ -19,8 +20,8 @@ def expand(word):
 def expand_text(text):
     # Look up contractions sequentially and replace them with expansions
     while True:
-        match = re.search(r'[ა-ჿ]+\.', text)
-        if bool(match) == False:
+        match = re.search(r'([ა-ჿ]+\.(?:[ა-ჿ]+\.)*)', text)
+        if not bool(match):
             break
 
         expansion = expand(text[match.start():match.end()])
@@ -28,3 +29,8 @@ def expand_text(text):
 
     return text
 
+
+def contract_text(text):
+    for key in emap.keys():
+        text = re.sub(key, emap[key], text)
+    return text
